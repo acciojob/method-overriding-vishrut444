0f19a7b3ce644f8f9cd5c607f3ec53a4f7654d1c
SNOW <visrut444@gmail.com>
2024-05-13 15:59:12 +0530

    method-overriding-solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,24 @@
 package com.driver;
 
 public class Main {
-  
+    //creating class a
+    public static class A{
+        public String meth(){
+            return "Invoking method from class A";
+        }
+    }
+    public static class B extends A{
+        @Override
+        public String meth(){
+            return "Invoking method from class B";
+        }
+    }
+    public static void main(String[] args) {
+        B obj = new B();
+        //method from class A is invoked
+        System.out.println(obj.meth());
+        //now method from class B
+        System.out.println(obj.meth());
+
+    }
 }
